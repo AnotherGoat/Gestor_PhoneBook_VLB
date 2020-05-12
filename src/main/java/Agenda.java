a9894ca8911d7ac0f24963f772cfc2dafef57359
VictorMardones <61598238+VictorMardones@users.noreply.github.com>
2020-05-12 15:13:27 -0400

    Añadido Scanner y método para validar la entrada de un int

--- src/main/java/Agenda.java
+++ src/main/java/Agenda.java
@@ -1,11 +1,15 @@
 
+// Importa la clase Scanner
+import java.util.Scanner;
 
 public class Agenda {
 
-    // Atributos
+    //// Atributos
     private String[] opciones = {"hola", "mundo", "Salir"}; // Arreglo con las opciones
+    Scanner teclado = new Scanner(System.in); // Scanner para recibir la entrada por teclado
+    int opcion; // Opción que ingresa el usuario
 
-    // Constructor
+    //// Constructores
     public Agenda() {
     }
 
@@ -20,8 +24,28 @@ public class Agenda {
             System.out.println(i+".- "+opciones[i-1]);
         }
 
+        this.opcion = validarInt();
+    }
 
+    //// Métodos de validación de entrada
+    // Valida un int
+    private int validarInt() {
+        boolean repetir = true; // Boolean para repetir en caso de ingresar una letra o símbolo
+        int a = 0; // Variable con la que se trabaja
 
+        while (repetir) {
+            try {
+                System.out.print("Ingrese la opción: ");
+                a = this.teclado.nextInt();
+                repetir = false;
+            } catch (Exception e) {
+                this.teclado.next();
+                System.out.println("Ingrese un número, por favor");
+                repetir = true;
+            }
+        }
+
+        return a;
     }
 
 }
