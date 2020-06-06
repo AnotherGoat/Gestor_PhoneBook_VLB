27f8d6f640daed0beda0623dff53f4d76abbb396
Bayron <bayron.m.1999@gmail.com>
2020-06-06 17:35:46 -0400

    se creo clase test

---
+++ src/test/java/AgendaTest.java
@@ -0,0 +1,52 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AgendaTest {
+
+    private Agenda agenda;
+
+    @Before
+    public void setUp() throws Exception {
+        agenda = new Agenda();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        agenda = null;
+    }
+
+    @Test
+    public void crearContacto() {
+    }
+
+    @Test
+    public void listarContactos() {
+    }
+
+    @Test
+    public void mostrarContacto() {
+    }
+
+    @Test
+    public void editarContacto() {
+    }
+
+    @Test
+    public void eliminarContacto() {
+    }
+
+    @Test
+    public void menuEdicion() {
+    }
+
+    @Test
+    public void crearOpciones() {
+    }
+
+    @Test
+    public void switchEdicion() {
+    }
+}
