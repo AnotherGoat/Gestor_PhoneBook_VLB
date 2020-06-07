505e970c4171fffaf06bc60d5dc701930d62800b
Victor <v,mardones04@ufromail.cl>
2020-06-06 23:38:42 -0400

    Se crearon los test unitarios

---
+++ src/test/java/AgendaTest.java
@@ -0,0 +1,48 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AgendaTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void crearContacto() {
+    }
+
+    @Test
+    public void listarContactos() {
+    }
+
+    @Test
+    public void mostrarContacto() {
+    }
+
+    @Test
+    public void editarContacto() {
+    }
+
+    @Test
+    public void eliminarContacto() {
+    }
+
+    @Test
+    public void menuEdicion() {
+    }
+
+    @Test
+    public void crearOpciones() {
+    }
+
+    @Test
+    public void switchEdicion() {
+    }
+}
---
+++ src/test/java/MenuTest.java
@@ -0,0 +1,28 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MenuTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void desplegarMenu() {
+    }
+
+    @Test
+    public void switchMenu() {
+    }
+
+    @Test
+    public void getSeleccion() {
+    }
+}
