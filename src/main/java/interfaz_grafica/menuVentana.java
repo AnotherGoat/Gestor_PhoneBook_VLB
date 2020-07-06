6bee75e4ce38f31e60dae07339475e1c7f15674c
Bayron <b.munoz15@ufromail.com>
2020-07-06 18:42:01 -0400

    Se crean botones en la ventana menu

--- src/main/java/interfaz_grafica/menuVentana.java
+++ src/main/java/interfaz_grafica/menuVentana.java
@@ -1,4 +1,13 @@
 package interfaz_grafica;
 
-public class menuVentana {
+import javax.swing.*;
+
+public class menuVentana extends JFrame {
+    private JButton nuevoContactoButton;
+    private JPanel panel1;
+    private JButton listaDeContactosButton;
+    private JButton detalleDeUnContactoButton;
+    private JButton editarContactoButton;
+    private JButton eliminarContactoButton;
+    private JButton salirButton;
 }
