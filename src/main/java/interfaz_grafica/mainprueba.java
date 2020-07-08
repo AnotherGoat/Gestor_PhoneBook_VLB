4da2f6bfc516165183924409ca7b4ff82cd3de85
Luisburgoro <64493029+Luisburgoro@users.noreply.github.com>
2020-07-08 00:48:00 -0400

    Se añadió método setResizable() para inhabilitar el boton minimizar

--- src/main/java/interfaz_grafica/mainprueba.java
+++ src/main/java/interfaz_grafica/mainprueba.java
@@ -4,5 +4,6 @@ public class mainprueba {
     public static void main(String[] args) {
         menuVentana prueba = new menuVentana();
         prueba.setVisible(true);
+        prueba.setResizable(false);//inhabilita el funcionamiento del boton minimizar
     }
 }
