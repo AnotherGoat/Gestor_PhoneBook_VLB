5a109770e03d0f7af5b4167a97b7a559e9616518
VictorMardones <v.mardones04@ufromail.cl>
2020-08-29 06:01:09 -0400

    Comenzar a hacer la interfaz gráfica

--- src/main/java/interfaz_grafica/MainPrueba.java
+++ src/main/java/interfaz_grafica/MainPrueba.java
@@ -1,11 +1,16 @@
 package interfaz_grafica;
 
+import json.GestorJSON;
+import org.json.JSONArray;
+import phonebook.Agenda;
+
 import java.awt.*;
 import java.net.URL;
 
+/**
+ * Launcher de prueba para la interfaz gráfica, copiando a Principal
+ */
 public class MainPrueba {
-
-
 
     public static void main(String[] args) {
         VentanaPrincipal vPrincipal = new VentanaPrincipal();
--- src/main/java/interfaz_grafica/VentanaPrincipal.java
+++ src/main/java/interfaz_grafica/VentanaPrincipal.java
@@ -1,49 +1,99 @@
 package interfaz_grafica;
 
-import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class VentanaPrincipal extends JFrame {
-    // Atributos de la ventana (todavía no tienen función los botones)
+
+    //// Atributos
+    /**
+     * Panel principal
+     */
+    private JPanel panel;
+    /**
+     * Label con el nombre del programa
+     */
+    private JLabel labelGestorPhoneBook;
+    /**
+     * Botón para crear un nuevo contacto
+     */
     private JButton botonNuevoContacto;
+    /**
+     * Botón para ver los datos de un contacto
+     */
     private JButton botonDatosContacto;
+    /**
+     * Botón para acceder al menú de edición de un contacto
+     */
     private JButton botonEditarContacto;
+    /**
+     * Botón para eliminar a un contacto
+     */
     private JButton botonEliminarContacto;
+    /**
+     * Botón para salir del programa
+     */
     private JButton botonSalir;
 
+    //// Constructores
     public VentanaPrincipal(){
-        setTitle("Gestor Phonebook VLB"); // Título
-        setSize(200, 200); // Se define el tamaño de ventana
+        iniciarVentanaPrincipal();
+
+        // Título
+        setTitle("Gestor Phonebook VLB");
+        setSize(200, 400); // Se define el tamaño de ventana
         setLocationRelativeTo(null); // Centrar la ventana
         setResizable(false); // Inhabilita el funcionamiento del botón maximizar
         setDefaultCloseOperation(EXIT_ON_CLOSE); // Cuando se cierre la ventana se finaliza el programa
-
-        cargarIcono();
-        iniciarBotones();
     }
 
+    //// Métodos
+    private void iniciarVentanaPrincipal(){
+        // Instancia el panel
+        panel = new JPanel();
+        //panel1.setLayout(new BoxLayout(panel1, BoxLayout.Y_AXIS));
+
+        // Instancia el label
+        labelGestorPhoneBook = new JLabel();
+        labelGestorPhoneBook.setText("Gestor PhoneBook VLB");
+
+        // Instancia los botones
+        botonNuevoContacto = new JButton();
+        botonNuevoContacto.setText("Crear un contacto nuevo");
+
+        botonDatosContacto = new JButton();
+        botonDatosContacto.setText("Ver datos de un contacto");
+
+        botonEditarContacto = new JButton();
+        botonEditarContacto.setText("Editar un contacto");
+
+        botonEliminarContacto = new JButton();
+        botonEliminarContacto.setText("Eliminar un contacto");
+
+        botonSalir = new JButton();
+        botonSalir.setText("Salir del programa");
+
+        // Añade el label y los botones al panel
+        panel.add(labelGestorPhoneBook);
+        panel.add(botonNuevoContacto);
+        panel.add(botonDatosContacto);
+        panel.add(botonEditarContacto);
+        panel.add(botonEliminarContacto);
+        panel.add(botonSalir);
+
+        // Añade el panel a la ventana
+        add(panel);
+
+        // Carga el ícono de la aplicación
+        cargarIcono();
+    }
+
+    /**
+     * Método que carga el ícono de la aplicación desde un archivo externo
+     */
     private void cargarIcono() {
         Image icono = Toolkit.getDefaultToolkit().getImage("archivos/icono_phonebook_nuevo.png");
         setIconImage(icono);
     }
-
-    private void iniciarBotones(){
-        JPanel panel1 = new JPanel();
-        //panel1.setLayout(new BoxLayout(panel1, BoxLayout.Y_AXIS));
-
-        botonNuevoContacto = new JButton("Crear un contacto nuevo");
-        botonDatosContacto = new JButton("Ver datos de un contacto");
-        botonEditarContacto = new JButton("Editar un contacto");
-        botonEliminarContacto = new JButton("Eliminar un contacto");
-        botonSalir = new JButton("Salir del programa");
-
-        panel1.add(botonNuevoContacto);
-        panel1.add(botonDatosContacto);
-        panel1.add(botonEditarContacto);
-        panel1.add(botonEliminarContacto);
-        panel1.add(botonSalir);
-
-        add(panel1);
-    }
-
 }
