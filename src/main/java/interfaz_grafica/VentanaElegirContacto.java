e87f25e79429f3ac11bb5003b5fdcc0c86e1cbb9
VictorMardones <v.mardones04@ufromail.cl>
2020-09-01 12:46:25 -0400

    Añadir ScrollPane para la lista de contactos

--- src/main/java/interfaz_grafica/VentanaElegirContacto.java
+++ src/main/java/interfaz_grafica/VentanaElegirContacto.java
@@ -20,9 +20,21 @@ public class VentanaElegirContacto extends JDialogGeneral {
      */
     private String texto;
     /**
+     * JPanel que contendrá los botones de cada contacto
+     */
+    private JPanel panelBotones;
+    /**
      * List con los botones que representan a cada contacto
      */
-    private List<JButton> botonesContactos = new ArrayList<>();
+    private List<JButton> lista_botones = new ArrayList<>();
+    /**
+     * Scroll para el panel con los botones
+     */
+    private JScrollPane scroll;
+    /**
+     * Panel principal (parte 2)
+     */
+    private JPanel panel2;
 
     //// Constructores
     public VentanaElegirContacto(String texto) {
@@ -35,7 +47,7 @@ public class VentanaElegirContacto extends JDialogGeneral {
         // Título
         setTitle("Elegir contacto");
         // Tamaño inicial
-        setSize(300, 700);
+        setSize(300, 400);
 
         configurar();
     }
@@ -46,6 +58,9 @@ public class VentanaElegirContacto extends JDialogGeneral {
 
         // Usa el BoxLayout (para mostrar los botones de arriba a abajo
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panelBotones = new JPanel();
+        panelBotones.setLayout(new BoxLayout(panelBotones, BoxLayout.Y_AXIS));
+        panel2 = new JPanel();
 
         // Instancia el label
         labelEscojaContacto = new JLabel(texto);
@@ -53,17 +68,25 @@ public class VentanaElegirContacto extends JDialogGeneral {
 
         // Instancia los botones
         botonVolverAtras.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel2.add(botonVolverAtras);
 
         for(String s : Principal.agenda.getLista_Nombres()){
-            botonesContactos.add(new JButton(s));
+            lista_botones.add(new JButton(s));
         }
+
+        for(JButton jb : lista_botones){
+            panelBotones.add(jb);
+            jb.setAlignmentX(Component.CENTER_ALIGNMENT);
+        }
+
+        // Configurar JScrollPane
+        scroll = new JScrollPane(panelBotones);
+        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 
         // Añade el label y los botones al panel
         panel.add(labelEscojaContacto);
-        for(JButton jb : botonesContactos){
-            panel.add(jb);
-            jb.setAlignmentX(Component.CENTER_ALIGNMENT);
-        }
+        panel.add(scroll);
         panel.add(botonVolverAtras);
 
         // Añade el panel a la ventana
